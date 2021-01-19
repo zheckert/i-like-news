b8b7088aa82b67598bd10cb3652c1a9652f9da64
Zach Heckert <zach@zachheckert.com>
2021-01-19 14:00:04 -0700

    Added routes

--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -1,3 +1,52 @@
 const express = require("express")
 const newsRouter = express.Router()
 const News = require("../client/models/news")
+
+newsRouter.get("/", (req, res, next) => {
+    News.find((error, news) => {
+        if(error){
+            res.status(500)
+            return next(error)
+        }
+        return res.status(200).send(news)
+    })
+})
+
+newsRouter.post("/", (req, res, next) => {
+    const newNewsPost = new News(req.body)
+    newNewsPost.save((error, savedPost) => {
+        if(error){
+            res.status(500)
+            return next(error)
+        }
+        return res.status(200).send(savedPost)
+    })
+})
+
+newsRouter.put("/newsId", (req, res, next) => {
+    News.findOneAndUpdate(
+        { _id: req.params.newsId },
+        req.body,
+        { new: true },
+        (error, editedPost) => {
+            if(error){
+                res.status(500)
+                return next(error)
+            }
+            return res.status(200).send(editedPost)
+        }
+    )
+})
+
+newsRouter.delete("/newsId", (req, res, next) => {
+    News.findOneAndDelete(
+        { _id: req.params.newsId },
+        (error, deletedPost) => {
+            if(error){
+                res.status(500)
+                return next(error)
+            }
+            return res.status(200).send(`"${deletedPost.title}" removed successfully!`)
+        }
+    )
+})
